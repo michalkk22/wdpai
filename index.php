53dc5b797b7691abfaba7a10849c7f5d8443207f
unknown <m.kolodziej2211@gmail.com>
2025-02-01 17:44:53 +0100

    added initial routing

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo 'dupa';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('main', 'DefaultController');
+Routing::run($path);
